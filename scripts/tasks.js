7910fb59679cee05abe30c3a7c7dea26d1fdddbe
guiparpineli <guiparpineli@hotmail.com>
2022-04-13 22:40:28 -0300

    tasks e logout almost done

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,4 +1,14 @@
 
+if ((localStorage.getItem('jwt') === null) || (localStorage.getItem('jwt') === '') || (localStorage.getItem('jwt') === undefined)) {
+    location.href = 'index.html';
+};
+
+const logout = document.getElementById('closeApp');
+
+logout.onclick = () => {
+    localStorage.removeItem('jwt');
+    location.href = 'index.html'
+}
 
 const obterUsuario = (jwt) => {
 
@@ -17,7 +27,7 @@ const obterUsuario = (jwt) => {
         });
 };
 
-var task = [];
+
 
 const obterTasks = (jwt) => {
     fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', {
@@ -29,17 +39,20 @@ const obterTasks = (jwt) => {
     })
         .then((resposta) => resposta.json())
         .then((tasks) => {
-             tasks.forEach(a => {
+            tasks.forEach(a => {
                 const skeleton = document.getElementById('skeleton')
                 const tarefa = document.createElement('li');
                 const notDone = document.createElement('div');
                 const descricao = document.createElement('div');
+                const id = document.createElement('p');
                 const p = document.createElement('p');
                 const p2 = document.createElement('p');
-            
+
                 const pDescription = document.createTextNode(a.description);
                 const pTime = document.createTextNode(a.createdAt);
-            
+                const pId = document.createTextNode(a.id);
+
+                id.appendChild(pId);
                 p.appendChild(pDescription);
                 p2.appendChild(pTime);
                 descricao.appendChild(p);
@@ -47,14 +60,17 @@ const obterTasks = (jwt) => {
                 notDone.appendChild(descricao);
                 tarefa.appendChild(notDone);
                 tarefa.appendChild(descricao);
-            
+                tarefa.appendChild(id);
+
                 descricao.classList.add("descricao");
                 notDone.classList.add("not-done");
                 p.classList.add("nome");
                 p2.classList.add("timestamp");
                 tarefa.classList.add("tarefa");
-            
-                skeleton.appendChild(tarefa);
+                id.style.display = 'none'
+
+                skeleton.prepend(tarefa);
+               
             }
             )
         }
@@ -62,9 +78,82 @@ const obterTasks = (jwt) => {
 }
 
 
+const makeTasks = (jwt) => {
+
+    const inpuTask = document.getElementById('novaTarefa')
+
+    const dataTask = {
+        description: inpuTask.value,
+        completed: false
+    };
+
+    fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json",
+            authorization: jwt
+        },
+        body: JSON.stringify(dataTask)
+    })
+        .then(function (response) { return response.json() })
+        .then(a => {
+            const skeleton = document.getElementById('skeleton')
+            const tarefa = document.createElement('li');
+            const notDone = document.createElement('div');
+            const descricao = document.createElement('div');
+            const p = document.createElement('p');
+            const p2 = document.createElement('p');
+
+            const pDescription = document.createTextNode(a.description);
+            const pTime = document.createTextNode(a.createdAt);
+
+            p.appendChild(pDescription);
+            p2.appendChild(pTime);
+            descricao.appendChild(p);
+            descricao.appendChild(p2);
+            notDone.appendChild(descricao);
+            tarefa.appendChild(notDone);
+            tarefa.appendChild(descricao);
+
+            descricao.classList.add("descricao");
+            notDone.classList.add("not-done");
+            p.classList.add("nome");
+            p2.classList.add("timestamp");
+            tarefa.classList.add("tarefa");
+
+            skeleton.prepend(tarefa);
+            inpuTask.value = ''
+        })
+}
+
+const delTask = (jwt) => {
+
+    const api = 'https://ctd-todo-api.herokuapp.com/v1/tasks/';
+    const url = api + id;
+
+    fetch(url, {
+        method: 'DELETE',
+        headers: {
+            "content-type": "application/json",
+            authorization: jwt
+        }
+    })
+}
 
 
 
+
+
+
+
+
+
+const submit = document.getElementById('btn-send');
+
+submit.onclick = a => {
+    a.preventDefault();
+    makeTasks(localStorage.getItem('jwt'));
+}
 
 window.onload = () => {
     obterUsuario(localStorage.getItem('jwt'))
